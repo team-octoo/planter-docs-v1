ae035930813fcecb4166e14e92e46e0d3d84a3d4
jessevandermeersch <jesse.vandermeersch@sweetmustard.be>
2024-03-21 17:24:22 +0100

    Theme switcher rework + mobile nav finish

---
+++ resources/js/theme_switcher.js
@@ -0,0 +1,38 @@
+console.log('hello switchers')
+const localStorage = window.localStorage
+const darkModeSwitches = document.querySelectorAll('#darkModeSwitch')
+const darkModeSwitchButtons = document.querySelectorAll('#darkModeSwitchButton')
+
+const list = document.querySelectorAll('#darkModeSwitchButton')
+
+const handleThemeSwitch = function () {
+  const darkMode = localStorage.getItem('darkMode')
+  if (darkMode === 'true') {
+    localStorage.setItem('darkMode', false)
+    darkModeSwitches.forEach((darkmodeSwitch) => {
+      darkmodeSwitch.checked = false
+    })
+  } else {
+    localStorage.setItem('darkMode', true)
+    darkModeSwitches.forEach((darkmodeSwitch) => {
+      darkmodeSwitch.checked = true
+    })
+  }
+}
+
+function initTheme() {
+  localStorage.setItem('darkMode', false)
+}
+
+function initEventListeners() {
+  darkModeSwitchButtons.forEach((button) => {
+    button.addEventListener('click', () => handleThemeSwitch())
+  })
+}
+
+function init() {
+  initTheme()
+  initEventListeners()
+}
+
+init()
