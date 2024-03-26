37c92784070cc67a4252b8a96694b45b23a9de85
jessevandermeersch <jesse.vandermeersch@sweetmustard.be>
2024-03-26 16:15:52 +0100

    Mobile docs navigation

--- resources/js/documentation_navigation.js
+++ resources/js/documentation_navigation.js
@@ -1,5 +1,8 @@
+const documentationNavigationWrapper = document.querySelector('.documentationNavigationWrapper')
+const mobileDocsNavOverlay = document.querySelector('#mobileDocsNavOverlay')
 const gettingStartedMenu = document.querySelector('#gettingStartedMenu')
 const communityMenu = document.querySelector('#gettingStartedMenu')
+const mobileNavToggle = document.querySelector('#mobileDocsNavButton')
 
 if (window.location.href.includes('docs')) {
   gettingStartedMenu.open = true
@@ -8,3 +11,16 @@ if (window.location.href.includes('docs')) {
 if (window.location.href.includes('community')) {
   communityMenu.open = true
 }
+
+function toggleMenu() {
+  console.log('toggle')
+  documentationNavigationWrapper.classList.toggle('--mobileOpenNav')
+  mobileDocsNavOverlay.classList.toggle('--showOverlay')
+}
+
+function addEventListeners() {
+  mobileNavToggle.addEventListener('click', () => toggleMenu())
+  mobileDocsNavOverlay.addEventListener('click', () => toggleMenu())
+}
+
+addEventListeners()
