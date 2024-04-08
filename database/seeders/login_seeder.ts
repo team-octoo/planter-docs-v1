436eb673895bf5c06b014e9eb07c035d48bcf288
Jos De Berdt <jozzeh@gmail.com>
2024-04-08 14:54:20 +0200

    check on admin user

--- database/seeders/login_seeder.ts
+++ database/seeders/login_seeder.ts
@@ -5,12 +5,15 @@ import { BaseSeeder } from '@adonisjs/lucid/seeders'
 export default class extends BaseSeeder {
   async run() {
     // Write your database queries inside the run method
-    await User.create({
-      firstName: 'Full',
-      lastName: 'Admin',
-      email: env.get('ADMIN_EMAIL'),
-      password: env.get('ADMIN_PASSWORD'),
-      active: true,
-    })
+    const adminUser = await User.findBy('email', env.get('ADMIN_EMAIL'))
+    if (!adminUser) {
+      await User.create({
+        firstName: 'Full',
+        lastName: 'Admin',
+        email: env.get('ADMIN_EMAIL'),
+        password: env.get('ADMIN_PASSWORD'),
+        active: true,
+      })
+    }
   }
 }
